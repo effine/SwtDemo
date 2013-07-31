9b226e1d292fed0e720dab00546727efca7b51cc
verphen <verphen@163.com>
2013-07-31 10:25:12 +0800

    测试 GitDemo 提交

---
+++ src/verphen/test/GitDemo.java
@@ -0,0 +1,12 @@
+/**
+ * @author Verphen
+ * @date 2013年7月31日  上午10:22:39
+ */
+
+package verphen.test;
+
+public class GitDemo {
+	public static void main(String[] args) {
+		System.out.println("测试Eclipse 提交代码到本地仓库");
+	}
+}
