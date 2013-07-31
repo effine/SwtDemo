8afdec4040ec253d1f106d090869ce8031b6d461
verphen <verphen@163.com>
2013-07-31 10:28:31 +0800

    push该文件GitHubDemo.java到GitHub远程仓库

---
+++ src/verphen/test/GitHubDemo.java
@@ -0,0 +1,14 @@
+/**
+ * @author Verphen
+ * @date 2013年7月31日  上午10:26:59
+ */
+
+package verphen.test;
+
+public class GitHubDemo {
+	public static void main(String[] args) {
+
+		System.out.println("测试push该文件到GitHub远程目录");
+
+	}
+}
