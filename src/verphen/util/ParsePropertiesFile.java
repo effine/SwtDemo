0b833b2d89dcb1efba71af46075bf4aad36797ea
verphen <verphen@163.com>
2013-07-31 17:40:12 +0800

    解析 properties 文件

---
+++ src/verphen/util/ParsePropertiesFile.java
@@ -0,0 +1,26 @@
+/**
+ * @author Verphen
+ * @date 2013年7月31日  下午5:02:35
+ */
+
+package verphen.util;
+
+import org.eclipse.osgi.util.NLS;
+
+public class ParsePropertiesFile extends NLS {
+
+	public static String FILENAME;
+	public static String USERNAME;
+
+	static {
+		NLS.initializeMessages(ParsePropertiesFile.class.getName(),
+				ParsePropertiesFile.class);
+	}
+
+	public static void main(String[] args) {
+
+		System.out.println(FILENAME + "     " + USERNAME);
+
+	}
+
+}
